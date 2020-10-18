fd30910602dd8402483c8174920e21280af782b0
MahmudAhmed <moe.purplefox@gmail.com>
2020-10-18 17:27:28 -0400

    push

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event.request))
-});
-
 var LINK_ONE = { "name" : "Google", "url" : "https://cloud.google.com/" };
 var LINK_TWO = { "name" : "Cloudflare", "url" : "https://cloudflare.com" };
 var LINK_THREE = { "name" : "Amazon", "url" : "https://aws.amazon.com/" };
+var LINK_FOUR = { "name" : "Microsoft", "url" : "https://azure.microsoft.com/en-us/services/cdn/" };
+var LINK_FIVE = { "name" : "Akamai", "url" : "https://www.akamai.com/" };
+var LINKS = [ LINK_ONE, LINK_TWO, LINK_THREE, LINK_FOUR, LINK_FIVE ]; 
+var SOCIALS = [ 
+  { "name": "Linkedin", "url" : "https://linkedin.com/in/mahmud-ahmed", "logo": "https://simpleicons.org/icons/linkedin.svg"}, 
+  { "name" : "Github", "url" : "https://github.com/mahmud-ahmed", "logo" : "https://simpleicons.org/icons/github.svg"},
+  { "name" : "Medium", "url" : "https://medium.com/@moe.purplefox", "logo" : "https://simpleicons.org/icons/medium.svg"}
+]
 
-var LINKS = [ LINK_ONE, LINK_TWO, LINK_THREE ]; 
+// Eventlistener
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request))
+});
 
 
 // Appends the Links to DIV#links tag 
@@ -18,19 +25,30 @@ class ElementHandler {
   }
 }
 
-// Removes the "style" attribute from the given div#profile tag. 
-class ProfileHandler {
+// Removes the "style" attribute from the given element. 
+class DisplayHandler {
+  constructor(ele) { // ele ==> the tag we are updating
+    this.ele = ele;
+  }
+
   async element(element) {
     element.removeAttribute("style");
+    if (this.ele === "social") { // are we updateing social links? 
+      for ( let social of SOCIALS) {
+        element.append(`<a href="${social.url}"><svg><image href=${social.logo} height="50" width="50"/></svg></a>`, { html: true })
+      }
+    } else if (this.ele === "background") {
+      element.setAttribute("style", "background-color: #2CA3DC")
+    }
   }
 }
 
 
-//Rewrites "src" and text for Avatar & Name
+//Rewrites "src"/text for elements
 class AttributeRewriter {
-  constructor(attributeName) {
+  constructor(attributeName, value) {
     this.attributeName = attributeName
-    this.value = attributeName === "src" ? "https://media-exp1.licdn.com/dms/image/C4D35AQGmLYdaPBUNvw/profile-framedphoto-shrink_400_400/0?e=1603137600&v=beta&t=MDYky6qyGQ1KsylwGErNgP9FRKxObb8LsMGbRQ5B3eQ" : "Mahmud"
+    this.value = attributeName === "src" ? "https://media-exp1.licdn.com/dms/image/C4D35AQGmLYdaPBUNvw/profile-framedphoto-shrink_400_400/0?e=1603137600&v=beta&t=MDYky6qyGQ1KsylwGErNgP9FRKxObb8LsMGbRQ5B3eQ" : value;
   }
   element(element) {
     if (this.attributeName === "src") {
@@ -58,9 +76,12 @@ async function handleRequest(request) {
     const res = await fetch("https://static-links-page.signalnerve.workers.dev");
     const rewriter = new HTMLRewriter()
       .on("#links", new ElementHandler())
-      .on("#profile", new ProfileHandler())
-      .on("#name", new AttributeRewriter("value"))
-      .on("#avatar", new AttributeRewriter("src"));
+      .on("#profile", new DisplayHandler())
+      .on("#name", new AttributeRewriter("value", "Mahmud Ahmed"))
+      .on("#avatar", new AttributeRewriter("src"))
+      .on("#social", new DisplayHandler("social"))
+      .on("body", new DisplayHandler("background"))
+      .on("title", new AttributeRewriter("value", "Mahmud Ahmed"));
 
     return rewriter.transform(res);
   };
